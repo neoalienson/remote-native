d4c38fe5384b808177ad61e9292bd44eed714641
Neo <neo@MacBook-Pro.local>
2015-04-10 06:57:11 -0700

    can change packager URL from Settings

--- iOS/AppDelegate.m
+++ iOS/AppDelegate.m
@@ -16,18 +16,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  NSURL *jsCodeLocation;
-
-  // Loading JavaScript code - uncomment the one you want.
-
-  // OPTION 1
-  // Load from development server. Start the server from the repository root:
-  //
-  // $ npm start
-  //
-  // To run on device, change `localhost` to the IP address of your computer, and make sure your computer and
-  // iOS device are on the same Wi-Fi network.
-  jsCodeLocation = [NSURL URLWithString:@"http://home.orz.hk:8086/index.ios.bundle"];
+  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+  NSString *packagerUrl = [defaults stringForKey:@"packager_url"];
+  NSURL *jsCodeLocation = [NSURL URLWithString:packagerUrl];
 
   RCTRootView* rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"RemoteNative"
